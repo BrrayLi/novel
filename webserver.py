4cfdaa36e886445e25f3aad0e48375ba428bc139
root <root@b07b91fe4b2d.(none)>
2019-01-21 07:47:25 +0000

    Add time tags

--- webserver.py
+++ webserver.py
@@ -1,5 +1,7 @@
 import socket
+import datetime
 import re
+import os
 
 HOST, PORT = '',80 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,22 +11,24 @@ listen_socket.listen(1)
 print 'Serving HTTP on port %s ...' % PORT
 path = ('/tdtsg','/qzfs')
 while True:
-    	
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    http_response=""
-    matchobj = re.finditer(r"GET (.*?) HTTP",request)
-    for result in matchobj:
-	request_path = result.group(1)
-    print request_path
-    if request_path in path:
-	file_path = "/tmp/novel" + request_path + ".html"
-        file_text = open(file_path,"r+")
-        http_response = "HTTP/1.1 200 OK\n\n" 
-        for text in file_text:
-	    http_response = http_response + text
-    else:
-	http_response ="HTTP/1.1 500 NOT FOUND\n"
+    try:	    	
+    	client_connection, client_address = listen_socket.accept()
+	request = client_connection.recv(1024)
+    	http_response=""
+ 	matchobj = re.finditer(r"GET (.*?) HTTP",request)
+        for result in matchobj:
+		request_path = result.group(1)
+        print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"  "+request_path
+	if request_path in path:
+		file_path = os.getcwd()  + request_path + ".html"
+        	file_text = open(file_path,"r+")
+        	http_response = "HTTP/1.1 200 OK\n\n" 
+        	for text in file_text:
+	    		http_response = http_response + text
+    	else:
+		http_response ="HTTP/1.1 500 NOT FOUND\n"
     #print http_response[0:20]
-    client_connection.sendall(http_response)
-    client_connection.close()
+        client_connection.sendall(http_response)
+        client_connection.close()
+    except BaseException,args:
+	print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+" Error:  "+repr(args)
